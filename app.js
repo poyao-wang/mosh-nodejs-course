585a9b047401218cff83a56c1b0d9277ff98a5f6
BoyoWang <michael90110@egmail.com>
2020-08-17 21:45:42 +0900

    02-10 File System Module

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const os = require("os");
+const fs = require("fs");
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+//Synchronous function
+// const files = fs.readdirSync("./");
+// console.log(files);
 
-console.log(`Total Memory : ${totalMemory}`);
-console.log(`Free Memory : ${freeMemory}`);
+//Asynchronous function
+fs.readdir("./", function (err, files) {
+  if (err) console.log("Error", err);
+  else console.log("Result", files);
+});
