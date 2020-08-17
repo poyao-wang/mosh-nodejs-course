3b5f8402c18d0b4b2aaf373fdf569c7990b94db7
BoyoWang <michael90110@egmail.com>
2020-08-17 22:26:23 +0900

    02-12 Event Arguments

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
 //Register a listener
-emitter.on("messageLogged", function () {
-  console.log("Listener called");
+emitter.on("messageLogged", (arg) => {
+  console.log("Listener called", arg);
 });
 
 // Raise and event
-emitter.emit("messageLogged");
+emitter.emit("messageLogged", { id: 1, url: "http://" });
