471a444fbed09461f8e9ff9c340a34a07200283d
BoyoWang <michael90110@egmail.com>
2020-08-18 16:35:58 +0900

    02-14-2 HTTP Module

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const http = require("http");
 
-const sever = http.createServer();
-
-sever.on("connection", (socket) => {
-  console.log("new connection");
+const sever = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World");
+    res.end();
+  }
+  if (req.url === "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
 });
 
 sever.listen(3000);
