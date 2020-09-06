304821fb4a43340b45e14ce31971c65aba17467c
BoyoWang <michael90110@gmail.com>
2020-09-06 14:02:04 +0900

    Change back to original

--- index.js
+++ index.js
@@ -28,10 +28,8 @@ async function createCourse() {
 
 async function getCourses() {
   const courses = await Course
-    // .find({ author: "Mosh", isPublished: true })
-    .find()
-    .or([{ author: "Mosh" }, { isPublished: true }])
-    .and([{ author: "Mosh" }, { isPublished: true }])
+    //
+    .find({ author: "Mosh", isPublished: true })
     .limit(10)
     .sort({ name: 1 }) // 1 for ascending, -1 for descending
     .select({ name: 1, tags: 1 });
