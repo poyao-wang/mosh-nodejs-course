0d29153892bd65809981faa3c1a0446853effc61
BoyoWang <michael90110@gmail.com>
2020-09-06 13:57:16 +0900

    07-10 Logical Query Operators

--- index.js
+++ index.js
@@ -27,7 +27,11 @@ async function createCourse() {
 }
 
 async function getCourses() {
-  const courses = await Course.find({ author: "Mosh", isPublished: true })
+  const courses = await Course
+    // .find({ author: "Mosh", isPublished: true })
+    .find()
+    .or([{ author: "Mosh" }, { isPublished: true }])
+    .and([{ author: "Mosh" }, { isPublished: true }])
     .limit(10)
     .sort({ name: 1 }) // 1 for ascending, -1 for descending
     .select({ name: 1, tags: 1 });
