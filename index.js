1ba9d4c7805c327260f0c3c4dc619130a5fbe177
BoyoWang <michael90110@gmail.com>
2020-09-06 14:21:59 +0900

    Change back

--- index.js
+++ index.js
@@ -29,14 +29,7 @@ async function createCourse() {
 async function getCourses() {
   const courses = await Course
     //
-    // .find({ author: "Mosh", isPublished: true })
-
-    .find({ author: /^Mosh/ })
-
-    .find({ author: /Hamadani$/i })
-
-    .find({ author: /.*Mosh.*/i })
-
+    .find({ author: "Mosh", isPublished: true })
     .limit(10)
     .sort({ name: 1 }) // 1 for ascending, -1 for descending
     .select({ name: 1, tags: 1 });
