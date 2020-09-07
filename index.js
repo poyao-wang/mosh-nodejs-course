cdffda5c0b77618e7f0215e7c0bb2d5851c3605c
BoyoWang <michael90110@gmail.com>
2020-09-07 23:14:57 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/mongo-exercises")
+  .then(() => console.log("Connected to Mongodb..."))
+  .catch((err) => console.log("Could not connect to MongoDB...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: Date,
+  isPublished: Boolean,
+  price: Number,
+});
+
+const Course = mongoose.model("Courses", courseSchema);
+
+async function getCourses() {
+  const courses = await Course
+    //
+    .find({ isPublished: true, tags: "backend" })
+    .sort({ name: 1 }) // 1 for ascending, -1 for descending
+    .select({ name: 1, author: 1 });
+  console.log(courses);
+}
+
+getCourses();
