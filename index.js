270473a53467c8a65a563936c1b09d5e6e680596
BoyoWang <michael90110@gmail.com>
2020-09-08 15:35:25 +0900

    07-18-2 Updating a Document- Update First

--- index.js
+++ index.js
@@ -27,15 +27,12 @@ async function getCourses() {
 }
 
 async function updateCourse(id) {
-  const result = await Course.update(
-    { _id: id },
-    {
-      $set: {
-        author: "Mosh",
-        isPublished: false,
-      },
-    }
-  );
+  const result = await Course.findByIdAndUpdate(id, {
+    $set: {
+      author: "Jack",
+      isPublished: true,
+    },
+  });
   console.log(result);
 }
 
