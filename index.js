e9d1796a80d4e2f8b4139c43109332394970fc5b
BoyoWang <michael90110@gmail.com>
2020-09-09 20:51:15 +0900

    fix connection warnings

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/playground")
+  .connect("mongodb://localhost/playground", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to Mongodb..."))
   .catch((err) => console.log("Could not connect to MongoDB...", err));
 
