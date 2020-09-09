c64484b6ff9a51a67e25713b5c6f697c6469ec5e
BoyoWang <michael90110@gmail.com>
2020-09-09 22:08:27 +0900

    08-07 Project- Add Persistence to Genres API

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const genres = require("./routes/genres");
+
+mongoose
+  .connect("mongodb://localhost/vidly", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Mongodb..."))
+  .catch((err) => console.log("Could not connect to MongoDB...", err));
 
 app.use(express.json());
 app.use("/api/genres", genres);
