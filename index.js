198a8ad6e2bd5464dce01b1b0db5172e633311f4
BoyoWang <michael90110@gmail.com>
2020-09-11 21:39:54 +0900

    09-06 Project- Build the Movies API

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
 
 mongoose
   .connect("mongodb://localhost/vidly", {
@@ -16,6 +17,7 @@ mongoose
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
