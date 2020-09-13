76d4b1d5983bd0e66b8f426c77e681a0eef3da72
BoyoWang <michael90110@gmail.com>
2020-09-13 19:09:29 +0900

    10-03 Registering Users

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
+const users = require("./routes/users");
 
 mongoose
   .connect("mongodb://localhost/vidly", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
+    autoIndex: false,
   })
   .then(() => console.log("Connected to Mongodb..."))
   .catch((err) => console.log("Could not connect to MongoDB...", err));
@@ -22,6 +24,7 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
