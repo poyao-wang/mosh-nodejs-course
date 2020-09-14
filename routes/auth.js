918e741db7df7e56fa4b9693a1bda0ec0a80e974
BoyoWang <michael90110@gmail.com>
2020-09-14 16:44:37 +0900

    10-06 Authenticating Users

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const bcrypt = require("bcrypt");
+const Joi = require("joi");
 const _ = require("lodash");
 const express = require("express");
-const { User, validate } = require("../models/user");
+const { User } = require("../models/user");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
@@ -9,15 +10,21 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send("User already registered.");
+  if (!user) return res.status(400).send("Invalid email or password.");
 
-  user = new User(_.pick(req.body, ["name", "email", "password"]));
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
 
-  await user.save();
-
-  res.send(_.pick(user, ["_id", "name", "email"]));
+  res.send(true);
 });
+
+function validate(req) {
+  const schema = {
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  };
+
+  return Joi.validate(req, schema);
+}
 
 module.exports = router;
