707f24897dd1415bae87491e6b481dbce58d02b0
BoyoWang <michael90110@gmail.com>
2020-09-15 20:27:53 +0900

    11-05 Express Async Errors

--- routes/genres.js
+++ routes/genres.js
@@ -1,17 +1,13 @@
-const asyncMiddleware = require("../middleware/async");
 const admin = require("../middleware/admin");
 const auth = require("../middleware/auth");
 const express = require("express");
 const { Genre, validate } = require("../models/genre");
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncMiddleware(async (req, res) => {
-    const genres = await Genre.find().sort("name");
-    res.send(genres);
-  })
-);
+router.get("/", async (req, res) => {
+  const genres = await Genre.find().sort("name");
+  res.send(genres);
+});
 
 router.post("/", auth, async (req, res) => {
   const { error } = validate(req.body);
