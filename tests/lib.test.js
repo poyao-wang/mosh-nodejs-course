f4cec61ecb36030974a4ae25fb020ec5845e4a62
BoyoWang <michael90110@gmail.com>
2020-09-20 14:09:19 +0900

    12-12-3 Testing Objects

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -35,6 +35,6 @@ describe("getCurrencies", () => {
 describe("getProducts", () => {
   it("should return the product with the given id", () => {
     const result = lib.getProduct(1);
-    expect(result).toMatchObject({ id: 1 });
+    expect(result).toHaveProperty("id", 1);
   });
 });
