c6377aecaefab9ba7950a9dfbcf79fe8ea9b05fc
BoyoWang <michael90110@gmail.com>
2020-09-20 14:37:31 +0900

    12-13 Testing Exceptions

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -38,3 +38,19 @@ describe("getProducts", () => {
     expect(result).toHaveProperty("id", 1);
   });
 });
+
+describe("registerUser", () => {
+  it("should throw if username is falsy", () => {
+    const args = [null, undefined, NaN, "", 0, false];
+    args.forEach((a) => {
+      expect(() => {
+        lib.registerUser(a);
+      }).toThrow();
+    });
+  });
+  it("should return a user object if valid username is passed", () => {
+    const result = lib.registerUser("mosh");
+    expect(result).toMatchObject({ username: "mosh" });
+    expect(result.id).toBeGreaterThan(0);
+  });
+});
