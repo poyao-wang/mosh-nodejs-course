aaede55af44998d6956c4a4ea1da14f51c7d9338
BoyoWang <michael90110@gmail.com>
2020-09-21 18:24:05 +0900

    13-05 Populating the Test Db

--- tests/intergration/genres.test.js
+++ tests/intergration/genres.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const { Genre } = require("../../models/genre");
 
 let sever;
 
@@ -6,13 +7,23 @@ describe("/api/genres", () => {
   beforeEach(() => {
     sever = require("../../index");
   });
-  afterEach(() => {
+  afterEach(async () => {
     sever.close();
+    await Genre.remove({});
   });
   describe("GET /", () => {
     it("should return all genres", async () => {
+      Genre.collection.insertMany([
+        //
+        { name: "genre1" },
+        { name: "genre2" },
+      ]);
+
       const res = await request(sever).get("/api/genres");
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some((g) => g.name === "genre1")).toBeTruthy();
+      expect(res.body.some((g) => g.name === "genre2")).toBeTruthy();
     });
   });
 });
