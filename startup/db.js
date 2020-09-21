c734950e3b629f8f809e452b38a409296e8f8e3f
BoyoWang <michael90110@gmail.com>
2020-09-21 15:08:59 +0900

    13-03 Setting Up the Test Db

--- startup/db.js
+++ startup/db.js
@@ -1,13 +1,15 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
+const config = require("config");
 
 module.exports = function () {
+  const db = config.get("db");
   mongoose
-    .connect("mongodb://localhost/vidly", {
+    .connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
       autoIndex: false,
     })
-    .then(() => winston.info("Connected to Mongodb..."));
+    .then(() => winston.info(`Connected to ${db}...`));
 };
