7e7dbd7eca072765acafd85e7a34436553f9909f
BoyoWang <michael90110@gmail.com>
2020-09-24 14:34:32 +0900

    13-15 Code Coverage

--- tests/intergration/genres.test.js
+++ tests/intergration/genres.test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const { Genre } = require("../../models/genre");
 const { User } = require("../../models/user");
+const mongoose = require("mongoose");
 
 let sever;
 
@@ -40,6 +41,12 @@ describe("/api/genres", () => {
 
     it("should return 404 if invalid id is passed", async () => {
       const res = await request(sever).get(`/api/genres/1`);
+      expect(res.status).toBe(404);
+    });
+
+    it("should return 404 if no genre with the given id exists", async () => {
+      const id = mongoose.Types.ObjectId();
+      const res = await request(sever).get(`/api/genres/${id}`);
       expect(res.status).toBe(404);
     });
   });
