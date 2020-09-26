eb7ace1478244635b4beba675de2b2940c91ba94
BoyoWang <michael90110@gmail.com>
2020-09-26 17:10:58 +0900

    14-04 Populating the Database

---
+++ tests/intergration/returns.test.js
@@ -0,0 +1,40 @@
+const { Rental } = require("../../models/rental");
+const mongoose = require("mongoose");
+describe("/api/returns", () => {
+  let sever;
+  let customerId;
+  let movieId;
+  let rental;
+
+  beforeEach(async () => {
+    sever = require("../../index");
+
+    customerId = mongoose.Types.ObjectId();
+    movieId = mongoose.Types.ObjectId();
+
+    rental = new Rental({
+      customer: {
+        _id: customerId,
+        name: "12345",
+        phone: "12345",
+      },
+      movie: {
+        _id: movieId,
+        title: "12345",
+        dailyRentalRate: 2,
+      },
+    });
+    await rental.save();
+  });
+
+  afterEach(async () => {
+    sever.close();
+    await Rental.remove({});
+  });
+
+  it("should work", async () => {
+    const result = await Rental.findById(rental._id);
+    // console.log(result);
+    expect(result).not.toBeNull();
+  });
+});
