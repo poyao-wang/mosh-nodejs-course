291a1f9d961ca65d8238e50c08689625cb69e5b2
BoyoWang <michael90110@gmail.com>
2021-06-06 15:57:14 +0900

    06-10-2 Running Promises in Parallel

--- promise-api.js
+++ promise-api.js
@@ -1,8 +1,8 @@
-const p1 = new Promise((resolve) => {
+const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log("Async operation 1...");
-    resolve(1);
-  }, 1000);
+    reject(new Error("Something went wrong..."));
+  }, 2000);
 });
 
 const p2 = new Promise((resolve) => {
@@ -12,4 +12,8 @@ const p2 = new Promise((resolve) => {
   }, 2000);
 });
 
-Promise.all([p1, p2]).then((result) => console.log(result));
+Promise.all([p1, p2])
+  .then((result) => console.log(result))
+  .catch((err) => {
+    console.log("Error", err.message);
+  });
