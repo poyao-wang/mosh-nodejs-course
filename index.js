d7da7a1048280998ddf3176b11546b2e9beadf66
BoyoWang <michael90110@gmail.com>
2021-06-06 15:37:48 +0900

    05-07-1 Configuration

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require("config");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const Joi = require("joi");
@@ -14,6 +15,10 @@ if (app.get("env") === "development") {
   console.log("Morgan enabled...");
 }
 app.use(logger);
+
+// Configuration
+console.log(`Application Name: ${config.get("name")}`);
+console.log(`Mail Sever: ${config.get("mail.host")}`);
 
 const courses = [
   { id: 1, name: "course1" },
