741f5eedde29180847fff38010b4abc0f900cf7c
BoyoWang <michael90110@gmail.com>
2021-06-06 15:57:14 +0900

    06-03-2 Callbacks

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 console.log("Before");
 getUser(1, (user) => {
   console.log(user);
+  getRepositories(user.gitHubUsername, (repos) => {
+    console.log(repos);
+  });
 });
 console.log("After");
 
@@ -8,5 +11,12 @@ function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading a user from a database...");
     callback({ id: id, gitHubUsername: "mosh" });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log("Reading user repositories...");
+    callback(["repo1", "repo2", "repo3"]);
   }, 2000);
 }
