0c7c51e2646d700cce04e1e61ca1608558a48775
BoyoWang <michael90110@gmail.com>
2021-06-06 15:57:14 +0900

    06-11 Async and Await

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-console.log("Before");
-getUser(1)
-  .then((user) => getRepositories(user.gitHubUsername))
-  .then((repos) => getCommits(repos[0]))
-  .then((commits) => console.log("Commits", commits))
-  .catch((err) => console.log("Error", err.message));
-console.log("After");
+// console.log("Before");
+// getUser(1)
+//   .then((user) => getRepositories(user.gitHubUsername))
+//   .then((repos) => getCommits(repos[0]))
+//   .then((commits) => console.log("Commits", commits))
+//   .catch((err) => console.log("Error", err.message));
+// console.log("After");
+
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log("Commits :", commits);
+  } catch (err) {
+    console.log("Error", err.message);
+  }
+}
+
+displayCommits();
 
 function getUser(id) {
   return new Promise((resolve, reject) => {
