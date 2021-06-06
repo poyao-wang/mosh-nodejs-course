c19984cc0497892416f5637f2757085b875d389c
BoyoWang <michael90110@gmail.com>
2021-06-06 15:37:48 +0900

    04-07-3 Route Parameters

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.get("/api/posts/:year/:month", (req, res) => {
-  res.send(req.params);
+  res.send(req.query);
 });
 
 const port = process.env.PORT || 3000;
