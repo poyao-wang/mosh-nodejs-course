467378882fa96e28216495cf7949af9172ca62d6
BoyoWang <michael90110@gmail.com>
2021-06-06 15:37:48 +0900

    04-12-1 Handling HTTP PUT Requests

--- index.js
+++ index.js
@@ -47,6 +47,26 @@ app.get("/api/courses/:id", (req, res) => {
   res.send(course);
 });
 
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("The course was not found.");
+  }
+
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  const result = Joi.validate(req.body, schema);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
