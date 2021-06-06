73345c50f894eb5e784cdcbf28cd4004093364a0
BoyoWang <michael90110@gmail.com>
2021-06-06 15:37:48 +0900

    05-03-1 Creating Custom Middleware

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const app = express();
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  console.log("logging...");
+  next();
+});
+
+app.use(function (req, res, next) {
+  console.log("Authenticating...");
+  next();
+});
+
 const courses = [
   { id: 1, name: "course1" },
   { id: 2, name: "course2" },
