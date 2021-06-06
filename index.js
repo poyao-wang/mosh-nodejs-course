32b6c3952fff349697d7465de2e7255250bc61b6
BoyoWang <michael90110@gmail.com>
2021-06-06 15:57:14 +0900

    Before changing to promises

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ getUser(1, (user) => {
   console.log(user);
   getRepositories(user.gitHubUsername, (repos) => {
     console.log(repos);
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    });
   });
 });
 console.log("After");
@@ -18,5 +21,12 @@ function getRepositories(username, callback) {
   setTimeout(() => {
     console.log("Reading user repositories...");
     callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
+}
+
+function getCommits(repo, callback) {
+  setTimeout(() => {
+    console.log("Calling GitHub API...");
+    callback(["commit"]);
   }, 2000);
 }
