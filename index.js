86e340c53d5413fdb08ff8b439ff8b2ff20f0969
BoyoWang <michael90110@gmail.com>
2021-06-06 15:57:14 +0900

    06-02 Patterns for Dealing with Asynchronous Code

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 console.log("Before");
-setTimeout(() => {
-  console.log("Reading a user from a database...");
-}, 2000);
+const user = getUser(1);
+console.log(user);
 console.log("After");
+
+function getUser(id) {
+  setTimeout(() => {
+    console.log("Reading a user from a database...");
+    return { id: id, gitHubUsername: "mosh" };
+  }, 2000);
+}
