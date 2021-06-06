0f89e939822c7d4f1f78d79a2369278db79b241a
BoyoWang <michael90110@gmail.com>
2021-06-06 15:57:14 +0900

    06-12 Exercise

--- exercise.js
+++ exercise.js
@@ -1,34 +1,57 @@
-getCustomer(1, (customer) => {
-  console.log("Customer: ", customer);
-  if (customer.isGold) {
-    getTopMovies((movies) => {
+// getCustomer(1, (customer) => {
+//   console.log("Customer: ", customer);
+//   if (customer.isGold) {
+//     getTopMovies((movies) => {
+//       console.log("Top movies: ", movies);
+//       sendEmail(customer.email, movies, () => {
+//         console.log("Email sent...");
+//       });
+//     });
+//   }
+// });
+
+async function notifyCustomer() {
+  try {
+    const customer = await getCustomer(1);
+    console.log("Customer: ", customer);
+    if (customer.isGold === true) {
+      const movies = await getTopMovies();
       console.log("Top movies: ", movies);
-      sendEmail(customer.email, movies, () => {
-        console.log("Email sent...");
-      });
-    });
+      await sendEmail(customer.email, movies);
+      console.log("Email sent...");
+    }
+  } catch (err) {
+    console.log(err.message);
   }
-});
-
-function getCustomer(id, callback) {
-  setTimeout(() => {
-    callback({
-      id: 1,
-      name: "Mosh Hamedani",
-      isGold: true,
-      email: "email",
-    });
-  }, 4000);
 }
 
-function getTopMovies(callback) {
-  setTimeout(() => {
-    callback(["movie1", "movie2"]);
-  }, 4000);
+getCustomerSendEmail();
+
+function getCustomer(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        id: 1,
+        name: "Mosh Hamedani",
+        isGold: true,
+        email: "email",
+      });
+    }, 2000);
+  });
 }
 
-function sendEmail(email, movies, callback) {
-  setTimeout(() => {
-    callback();
-  }, 4000);
+function getTopMovies() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(["movie1", "movie2"]);
+    }, 2000);
+  });
+}
+
+function sendEmail(email, movies) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
 }
